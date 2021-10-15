15e45be1ec18f444622425a6c85b00a860c0b2b0
unknown <nilayd@nycstudents.net>
2021-10-14 23:36:54 -0400

    3

--- script.js
+++ script.js
@@ -5,7 +5,9 @@ let container = document.getElementById('container')
 let intro = document.getElementById('intro')
 let firstPage = true;
 let temp;
+let scrollBar = document.getElementById('scrollbar')
 container.classList.add('invis')
+container.remove()
 window.onload = function () {
   header.classList.add('shift')
 }
@@ -17,22 +19,25 @@ window.addEventListener('wheel', event => {
     bar.classList.remove('stick')
   }
   if (event.deltaY > 0 && firstPage) {
-    container.ontransitionend = ()=>{
+    container.ontransitionend = () => {
       console.log()
     }
+    scrollBar.style.top = '48vh'
     firstPage = false
     header.classList.add('dip')
     header.ontransitionend = () => {
       header.classList.remove('shift')
       header.classList.add('invis')
+      document.body.appendChild(container)
       container.classList.remove('invis')
       container.style.setProperty('--direction', '-60px')
       setTimeout(() => {
-        // container.style.marginTop = '25vh'
-        // container.style.transform = 'translateY(var(--direction))'
-        // container.style.opacity = '1'
         container.classList.add('introduce')
         intro.style.opacity = '1'
+        document.querySelectorAll('.img-containers')[0].style.opacity = '1'
+        document.querySelectorAll('.img-containers')[1].style.opacity = '1'
+
+
       }, 50)
 
     }
@@ -42,14 +47,16 @@ window.addEventListener('wheel', event => {
     header.ontransitionend = () => {
       console.log()
     }
+    scrollBar.style.top = '43px'
     firstPage = true
     container.style.setProperty('--direction', '60px')
-    // container.style.transform = 'translateY(var(--direction))'
-    // container.style.opacity = '0'
     container.classList.remove('introduce')
     intro.style.opacity = '0'
+    document.querySelectorAll('.img-containers')[0].style.opacity = '0'
+    document.querySelectorAll('.img-containers')[1].style.opacity = '0'
+
     container.ontransitionend = () => {
-      // container.style.marginTop = '0vh'
+      container.remove()
       container.classList.add('invis')
       header.classList.remove('dip')
       header.classList.remove('invis')
